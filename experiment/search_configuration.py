9530ecda0dabf5a4d337858e75c39f5be827420b
Lev Sorokin <sorokin@fortiss.org>
2023-01-07 23:51:06 +0100

    Clean up

--- experiment/search_configuration.py
+++ experiment/search_configuration.py
@@ -2,13 +2,14 @@ class SearchConfiguration(object):
     n_generations = None 
     population_size = None
     maximal_execution_time = None
-    max_tree_iterations = None
     num_offsprings = None
     prob_crossover = None
     eta_crossover = None
     prob_mutation = None
     eta_mutation = None
+    
     # NSGAII-DT specific
+    max_tree_iterations = None
     inner_num_gen = None
 
 #TODO create a search configuration file specific for each algorithm
@@ -16,11 +17,12 @@ class DefaultSearchConfiguration(SearchConfiguration):
     n_generations = 5 
     population_size = 20
     maximal_execution_time = None
-    max_tree_iterations = 4
     num_offsprings = None
     prob_crossover = 0.7
     eta_crossover = 20
     prob_mutation = None
     eta_mutation = 15
+
     # NSGAII-DT specific
     inner_num_gen = 4
+    max_tree_iterations = 4
