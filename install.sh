9b97d58ee4e4e8dd580db480660f110fd694d20c
openscenario <sorokin@fortiss.org>
2023-01-07 13:42:42 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+# creates a virtual environment and installs dependencies 
+# in the virtual environment
+
+python -m virtualenv venv
+source ./venv/bin/activate
+python -m pip install -r requirements.txt
