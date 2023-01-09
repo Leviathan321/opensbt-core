e50a0ffdc783f547d157527d76d09890de65381a
Lev Sorokin <sorokin@fortiss.org>
2023-01-09 16:39:56 +0100

    Clean up

--- test.py
+++
@@ -1,9 +0,0 @@
-import importlib
-#module_name= "experiment.experiment"
-
-#modules= ["pymoo.algorithms.moo.nsga2"]
-module_name = "pymoo.algorithms.moo.nsga2"
-
-class_name = "Individual"
-MyClass = getattr(importlib.import_module(module_name), class_name)
-instance = MyClass()
