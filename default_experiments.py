1a7155dfab85f3d31a586eddb139ffbbd9c5c5d9
Lev Sorokin <sorokin@fortiss.org>
2023-02-26 10:52:36 +0000

    Rm carla experiments

--- default_experiments.py
+++ default_experiments.py
@@ -5,88 +5,6 @@ from problem.pymoo_test_problem import PymooTestProblem
 from experiment.experiment_store import *
 from algorithm.algorithm import *
 from evaluation.critical import *
-# from simulation.carla_simulation import CarlaSimulator
-
-# '''
-# EXAMPLE CARLA SIMULATOR
-# ego speed is in km/h
-# '''
-
-# def getExp1() -> Experiment:
-#     problem = ADASProblem(
-#                           problem_name="PedestrianCrossingStartWalk",
-#                           scenario_path=os.getcwd() + "/scenarios/PedestrianCrossing.xosc",
-#                           xl=[0.5, 1, 0],
-#                           xu=[3, 80, 60],
-#                           simulation_variables=[
-#                               "PedestrianSpeed",
-#                               "FinalHostSpeed",
-#                               "PedestrianEgoDistanceStartWalk"],
-#                           fitness_function=FitnessMinDistanceVelocityFrontOnly(),  
-#                           critical_function=CriticalAdasFrontCollisions(),
-#                           simulate_function=CarlaSimulator.simulate,
-#                           simulation_time=10,
-#                           sampling_time=100,
-#                           approx_eval_time=10,
-#                           do_visualize = False
-#                           )
-#     experiment = Experiment(name="1",
-#                             problem=problem,
-#                             algorithm=AlgorithmType.NSGAII,
-#                             search_configuration=DefaultSearchConfiguration())
-#     return experiment
-
-# experiments_store.register(getExp1())
-
-# '''
-#     BNH Problem
-
-#     Pareto solutions:
-#     x∗1=x∗2∈[0,3]  and x∗1∈[3,5], x∗2=3
-# '''
-
-# def getExp2() -> Experiment:
-#     problem = PymooTestProblem(
-#         'BNH',
-#         critical_function=CriticalBnhDivided())
-
-#     config = DefaultSearchConfiguration()
-#     config.maximal_execution_time = "00:00:01"
-#     experiment = Experiment(name="2",
-#                             problem=problem,
-#                             algorithm=AlgorithmType.NSGAII,
-#                             search_configuration=config)
-
-#     return experiment
-
-# experiments_store.register(getExp2())
-
-# def getExp3() -> Experiment:
-#     problem = ADASProblem(
-#                           problem_name="TwoPedestriansCrossing",
-#                           scenario_path=os.getcwd() + "/scenarios/PedestrianCrossingSecond.xosc",
-#                           xl=[0.5, 1, 0],
-#                           xu=[3, 80, 60],
-#                           simulation_variables=[
-#                               "PedestrianSpeed",
-#                               "FinalHostSpeed",
-#                               "PedestrianEgoDistanceStartWalk"],
-#                           fitness_function=FitnessMinDistanceVelocityFrontOnly(),  
-#                           critical_function=CriticalAdasFrontCollisions(),
-#                           simulate_function=CarlaSimulator.simulate,
-#                           simulation_time=10,
-#                           sampling_time=100,
-#                           approx_eval_time=10
-#                           )
-#     config = DefaultSearchConfiguration()
-#     experiment = Experiment(name="3",
-#                             problem=problem,
-#                             algorithm=AlgorithmType.NSGAII,
-#                             search_configuration=config)
-
-#     return experiment
-    
-# experiments_store.register(getExp3())
 
 '''
 Rastrigin SOO problem (n_var = 2, n_obj = 1), test for PSO
@@ -107,7 +25,7 @@ def getExp4() -> Experiment:
     
 experiments_store.register(getExp4())
 
-''' Reflection study experiment for search with NSGAII-DT (ISTJ Paper) '''
+''' FOCETA Prescan experiment for search with NSGAII'''
 def getExp5() -> Experiment:
     from simulation.prescan_simulation import PrescanSimulator
 
@@ -125,7 +43,7 @@ def getExp5() -> Experiment:
                           simulate_function=PrescanSimulator.simulate,
                           simulation_time=10,
                           sampling_time=100,
-                          problem_name="Demo_AVP_Reflection",
+                          problem_name="Demo_AVP_Pedestrian",
                           approx_eval_time=30)
 
     config = DefaultSearchConfiguration()
