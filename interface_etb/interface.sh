ce25c662be540445eb6d1a6f82987ecbcd6ab748
Lev <sorokin@fortiss.org>
2023-08-25 16:09:27 +0200

    Update

--- interface_etb/interface.sh
+++ interface_etb/interface.sh
@@ -3,8 +3,7 @@
 cd ..
 source venv/Scripts/activate
 
-scenario=$1 #"C:\Users\sorokin\Documents\Projects\testing\FOCETA\experiments\Leuven_PedestrianCrossing\Leuven_AVP_ori\ChangeModel.pb"
-# echo "Read scenario: "$scenario
+scenario=$1 
 n_arga=`expr $# - 1`
 n_dim=`expr $n_arga / 2` # get the number of dimensions
 # read search space (we assume 2-2+n_dim and 5-5+n_dim  are min and upper bounds respectively)
@@ -20,6 +19,7 @@ echo $(tail -n 1 $file | grep -oP '^INFO:root:critical_testcases:\s*\K.*' )
 #rm interface_etb/output_opensbt.txt
  
 ################# 
+
 # Example 
-# $ bash test_interface.sh 2 1 2 3 11 12 12
-# C:\Users\Lev\Documents\fortiss\projects\foceta\SBT-research/results/single/BNH\NSGA-II\temp\all_critical_testcases.csv
+# $ bash test_interface.sh 1 1 2 3 11 12 12
+# C:\Users\Lev\Documents\fortiss\projects\testing\open-sbt\opensbt-core/results/BNH\NSGA2\25-08-2023_15-58-38\all_critical_testcases.csv
