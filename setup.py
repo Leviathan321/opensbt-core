e035e4d39748f9501eaf4b57be2e76f84a320700
openscenario <sorokin@fortiss.org>
2024-01-12 11:39:51 +0100

    Update.

--- setup.py
+++ setup.py
@@ -2,6 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='opensbt',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'deap==1.4.1',
+        'numpy==1.24.4',
+        'scipy==1.10.1',
+        'matplotlib==3.7.4',
+        'docker==6.1.3',
+        'pandas==2.0.3',
+        'setuptools',
+        'scikit-learn==1.3.2',
+        'pymoo==0.6.0.1'
+    ],
+    python_requires=">=3.8"
 )
